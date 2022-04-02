967021a4f5e7ca7eae074585f2f8b1700998ea0a
matcha <matchadcaf@protonmail.com>
2022-04-02 06:54:52 +0400

    fix: eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    "airbnb-base",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   rules: {},
 };
