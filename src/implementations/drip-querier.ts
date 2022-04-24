71e7f39d102dc3c6859c6c14e045e307d7806df4
matcha <matchadcaf@protonmail.com>
2022-04-24 09:44:11 +0400

    feat: getAllTokenAs and getAllTokenBs

--- src/implementations/drip-querier.ts
+++ src/implementations/drip-querier.ts
@@ -1,7 +1,7 @@
 import { Address, Program, Provider } from '@project-serum/anchor';
 import { PublicKey } from '@solana/web3.js';
 import { Configs } from '../config';
-import { Vault, VaultProtoConfig, Token } from '../config/types';
+import { Vault, Token } from '../config/types';
 import { DcaVault } from '../idl/type';
 import DcaVaultIDL from '../idl/idl.json';
 import { DripQuerier } from '../interfaces';
@@ -20,20 +20,46 @@ export class DripQuerierImpl implements DripQuerier {
     this.vaultProgram = new Program(DcaVaultIDL as DcaVault, config.vaultProgramId, provider);
   }
 
-  getAllVaults(): Promise<Vault[]> {
+  public async getAllVaults(): Promise<Record<string, Vault>> {
+    return Configs[this.network].vaults;
+  }
+
+  public async getAllPositions(): Promise<Record<string, VaultPositionAccount>> {
     throw new Error('Method not implemented.');
   }
 
-  getAllPositions(): Promise<VaultProtoConfig[]> {
-    throw new Error('Method not implemented.');
+  public async getAllTokenAs(givenTokenB?: PublicKey): Promise<Record<string, Token>> {
+    const vaults = Configs[this.network].vaults;
+    const tokenAPubkeys = (
+      givenTokenB
+        ? Object.values(vaults).filter((vault) => vault.tokenBMint.equals(givenTokenB))
+        : Object.values(vaults)
+    ).map((vault) => vault.tokenAMint);
+
+    return tokenAPubkeys.reduce(
+      (map, tokenPubkey) => ({
+        ...map,
+        [tokenPubkey.toBase58()]: Configs[this.network].tokens[tokenPubkey.toBase58()],
+      }),
+      {} as Record<string, Token>
+    );
   }
 
-  getAllTokenAs(givenB?: PublicKey): Promise<Token[]> {
-    throw new Error('Method not implemented.');
-  }
+  public async getAllTokenBs(givenTokenA?: PublicKey): Promise<Record<string, Token>> {
+    const vaults = Configs[this.network].vaults;
+    const tokenBPubkeys = (
+      givenTokenA
+        ? Object.values(vaults).filter((vault) => vault.tokenAMint.equals(givenTokenA))
+        : Object.values(vaults)
+    ).map((vault) => vault.tokenBMint);
 
-  getAllTokenBs(givenA?: PublicKey): Promise<Token[]> {
-    throw new Error('Method not implemented.');
+    return tokenBPubkeys.reduce(
+      (map, tokenPubkey) => ({
+        ...map,
+        [tokenPubkey.toBase58()]: Configs[this.network].tokens[tokenPubkey.toBase58()],
+      }),
+      {} as Record<string, Token>
+    );
   }
 
   fetchVaultProtoConfigAccounts(...pubkeys: Address[]): Promise<VaultAccount[]> {
--- src/interfaces/drip-querier/index.ts
+++ src/interfaces/drip-querier/index.ts
@@ -1,19 +1,12 @@
 import { Address } from '@project-serum/anchor';
 import { PublicKey } from '@solana/web3.js';
-import {
-  Token,
-  Vault,
-  VaultAccount,
-  VaultPeriodAccount,
-  VaultPositionAccount,
-  VaultProtoConfig,
-} from './results';
+import { Token, Vault, VaultAccount, VaultPeriodAccount, VaultPositionAccount } from './results';
 
 export interface DripQuerier {
-  getAllVaults(): Promise<Vault[]>;
-  getAllPositions(): Promise<VaultProtoConfig[]>;
-  getAllTokenAs(givenB?: PublicKey): Promise<Token[]>;
-  getAllTokenBs(givenA?: PublicKey): Promise<Token[]>;
+  getAllVaults(): Promise<Record<string, Vault>>;
+  getAllPositions(): Promise<Record<string, VaultPositionAccount>>;
+  getAllTokenAs(givenTokenB?: PublicKey): Promise<Record<string, Token>>;
+  getAllTokenBs(givenTokenA?: PublicKey): Promise<Record<string, Token>>;
 
   fetchVaultProtoConfigAccounts(...pubkeys: Address[]): Promise<VaultAccount[]>;
   fetchVaultAccounts(...pubkeys: Address[]): Promise<VaultAccount[]>;
