8a1f0c51e5d5b11ea5062b78a961c682e7b683df
matcha <matchadcaf@protonmail.com>
2022-04-24 16:11:20 +0400

    chore: export helpers

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './drip-client';
 export * from './interfaces';
 export * from './config';
 export * from './constants';
+export * from './helpers';
 
 // TODO: Support WSOL
 // TODO: TXs returned from anchor is probably signed(?); do the same with raw TXs created and returned by us
