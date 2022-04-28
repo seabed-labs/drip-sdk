9c7a37516aee7ebda31b8f53b5b8be973141a1d5
Mocha Dcaf <dcafmocha@protonmail.com>
2022-04-27 21:37:31 -0400

    chore: update tokens.ts

--- src/config/devnet/tokens.ts
+++ src/config/devnet/tokens.ts
@@ -2,16 +2,16 @@ import { PublicKey } from '@solana/web3.js';
 import { Token } from '../types';
 
 export const tokens: Record<string, Token> = {
-  EpurvB9cZ9WkcFePxH7V8vNpZy3nGQLsp2gcGehAWcAL: {
-    mint: new PublicKey('EpurvB9cZ9WkcFePxH7V8vNpZy3nGQLsp2gcGehAWcAL'),
+  BfqATYbPZJFdEdYWkEbFRBnhv1LB6wtLn299HjMmE4uq: {
+    mint: new PublicKey('BfqATYbPZJFdEdYWkEbFRBnhv1LB6wtLn299HjMmE4uq'),
     symbol: 'SOL',
   },
-  '31nFDfb3b4qw8JPx4FaXGgEk8omt7NuHpPkwWCSym5rC': {
-    mint: new PublicKey('31nFDfb3b4qw8JPx4FaXGgEk8omt7NuHpPkwWCSym5rC'),
+  ASuqwxvC4FXxJGT9XqZMXbCKDQBaRTApEhN2oN3VL3A8: {
+    mint: new PublicKey('ASuqwxvC4FXxJGT9XqZMXbCKDQBaRTApEhN2oN3VL3A8'),
     symbol: 'USDC',
   },
-  '9gAMFwMubEvQi6Tts5ePezJp6BqD72FVWECVgaNh9BpG': {
-    mint: new PublicKey('9gAMFwMubEvQi6Tts5ePezJp6BqD72FVWECVgaNh9BpG'),
+  '54uPbRrVwYsvZZMmsCXjLCtshgTquBWDZTNTJNbEDZ4H': {
+    mint: new PublicKey('54uPbRrVwYsvZZMmsCXjLCtshgTquBWDZTNTJNbEDZ4H'),
     symbol: 'ETH',
   },
 };
