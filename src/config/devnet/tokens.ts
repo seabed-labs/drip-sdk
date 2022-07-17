029fa05625b573f034b5879bf68ced687d18d57e
Mocha Dcaf <dcafmocha@protonmail.com>
2022-07-17 01:05:08 -0400

    fix: update devnet config

--- src/config/devnet/tokens.ts
+++ src/config/devnet/tokens.ts
@@ -2,12 +2,20 @@ import { PublicKey } from '@solana/web3.js';
 import { Token } from '../types';
 
 export const tokens: Record<string, Token> = {
-  GAstZSga54WrsQdDZ9pgch6Xe9Lyxaiyx4sY23nKyEZ4: {
-    mint: new PublicKey('GAstZSga54WrsQdDZ9pgch6Xe9Lyxaiyx4sY23nKyEZ4'),
-    symbol: 'SOL',
-  },
-  E3R1FopVaFpPAXojjdhJvWfKyaPuGHrx2GY7RCvHn7Dy: {
-    mint: new PublicKey('E3R1FopVaFpPAXojjdhJvWfKyaPuGHrx2GY7RCvHn7Dy'),
+  BzTNAVYYHn9aSfUKeNQ2Fv635MsY6WnHMVRMZgb4hbc6: {
+    mint: new PublicKey('BzTNAVYYHn9aSfUKeNQ2Fv635MsY6WnHMVRMZgb4hbc6'),
     symbol: 'USDC',
+  },
+  DqakM9iwYs425rToh9LCXzfzc1Xh3A3nrz36QkSJTfNd: {
+    mint: new PublicKey('DqakM9iwYs425rToh9LCXzfzc1Xh3A3nrz36QkSJTfNd'),
+    symbol: 'ETH',
+  },
+  EMtrTfsR7FvvJFg8J6g9y332L4PGzo3qDSztQbvbswpg: {
+    mint: new PublicKey('EMtrTfsR7FvvJFg8J6g9y332L4PGzo3qDSztQbvbswpg'),
+    symbol: 'BTC',
+  },
+  '8LXJFDvW9nrp4MXb4rSW7eSvxAhdzmdiUpNHiDdzJaNP': {
+    mint: new PublicKey('8LXJFDvW9nrp4MXb4rSW7eSvxAhdzmdiUpNHiDdzJaNP'),
+    symbol: 'TSOL',
   },
 };
--- src/config/devnet/vault-proto-configs.ts
+++ src/config/devnet/vault-proto-configs.ts
@@ -2,8 +2,12 @@ import { PublicKey } from '@solana/web3.js';
 import { VaultProtoConfig } from '../types';
 
 export const vaultProtoConfigs: Record<string, VaultProtoConfig> = {
-  '6E9JT6hW92Mf6DioroeC6xbPxbjWPn7NAM1J2an2TLLi': {
-    pubkey: new PublicKey('6E9JT6hW92Mf6DioroeC6xbPxbjWPn7NAM1J2an2TLLi'),
+  '22FnWdXzQ1tTrvjFaZ2S7Qs3RmGHbZtEr7jVjWFebrrv': {
+    pubkey: new PublicKey('22FnWdXzQ1tTrvjFaZ2S7Qs3RmGHbZtEr7jVjWFebrrv'),
     granularity: 60,
+  },
+  J6wYVgtJek7ady5wzvtZF6ivF26YqMHtxbTXfzSPZGFp: {
+    pubkey: new PublicKey('J6wYVgtJek7ady5wzvtZF6ivF26YqMHtxbTXfzSPZGFp'),
+    granularity: 3600,
   },
 };
