39037214e0c77a328f4fa7bcbc8c84c58d096294
matcha <matchadcaf@protonmail.com>
2022-08-12 00:53:38 -0700

    USDC -> BTC daily vault

--- src/config/mainnet/vaults.ts
+++ src/config/mainnet/vaults.ts
@@ -22,4 +22,14 @@ export const vaults: Record<string, Vault> = {
     tokenBAccount: new PublicKey('VWzfotdnp5C8BPPznyrEiJkVBfAhWp676jmeX2KppAG'),
     treasuryTokenBAccount: new PublicKey('qNP5HfgrTtW1iLd8ddPyYkqa6RkPP93dbgBUTzYoix6'),
   },
+  CBRZk2Agsb2E8rEcGPojJFv4K8hihA3MmYAJRYAxNy8K: {
+    // USDC -> BTC @ daily
+    pubkey: new PublicKey('CBRZk2Agsb2E8rEcGPojJFv4K8hihA3MmYAJRYAxNy8K'),
+    protoConfig: new PublicKey('6i3GfRb48FtUYB1e3UqfamT1D52UtGtTkF6tMF1F6YHv'),
+    tokenAMint: new PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'),
+    tokenBMint: new PublicKey('9n4nbM75f5Ui33ZbPYXn59EwSgE8CGsHtAeTH5YFeJ9E'),
+    tokenAAccount: new PublicKey('AJw72xgLP3Lx2iSKS2xVW838rNVU2temwGqh5hbofiQM'),
+    tokenBAccount: new PublicKey('3prjfBMhHRTU4FbtMMpJ9PRezy46pPJu3ePg8kHaFESP'),
+    treasuryTokenBAccount: new PublicKey('EcbzLF4pjPvHX7ARCCbqonA86uNUWVXoUAfNxpS7CCM6'),
+  },
 };
