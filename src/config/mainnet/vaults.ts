875bf0d476c03f64ab8845d6c6f1d8f19ee7d171
matcha <matchadcaf@protonmail.com>
2022-08-12 00:46:35 -0700

    Add USDC -> ETH drip vault

--- src/config/mainnet/vaults.ts
+++ src/config/mainnet/vaults.ts
@@ -12,4 +12,14 @@ export const vaults: Record<string, Vault> = {
     tokenBAccount: new PublicKey('GffoAkQZqq5jfYrp1RTVhWCywnGKdsAx47K3BQFwroKH'),
     treasuryTokenBAccount: new PublicKey('4E5WgXLJ4iiUw6pj2WiJDPqo2fPr4bXfG98JyPkUz2AZ'),
   },
+  BrkNC3vpj17h8hwDoqyYrCvEF1BqV6wNQLxP4DfhBiLb: {
+    // USDC -> ETH @ daily
+    pubkey: new PublicKey('BrkNC3vpj17h8hwDoqyYrCvEF1BqV6wNQLxP4DfhBiLb'),
+    protoConfig: new PublicKey('6i3GfRb48FtUYB1e3UqfamT1D52UtGtTkF6tMF1F6YHv'),
+    tokenAMint: new PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'),
+    tokenBMint: new PublicKey('7vfCXTUXx5WJV5JADk17DUJ4ksgau7utNKj4b963voxs'),
+    tokenAAccount: new PublicKey('BaztmqAZQLLPtAfTVD1NvuGVX2swAoQ1CMxEojE8yYkm'),
+    tokenBAccount: new PublicKey('VWzfotdnp5C8BPPznyrEiJkVBfAhWp676jmeX2KppAG'),
+    treasuryTokenBAccount: new PublicKey('qNP5HfgrTtW1iLd8ddPyYkqa6RkPP93dbgBUTzYoix6'),
+  },
 };
